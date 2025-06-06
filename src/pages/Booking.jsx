8620f866c500dc5e46f95f9537e718356a38a864
lisazumana03 <zumanalisa@gmail.com>
2025-06-06 18:28:25 +0200

    file incoming

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -3,14 +3,16 @@ Lisakhanya Zumana (230864821)
 Date: 05/06/2025
  */
 import React from 'react';
+import './Booking.css';
 
 function Booking() {
     return (
         <div className="booking-page">
+            <h1>Make your booking</h1>
             <form>
                 <label>Start Date(pick up date): </label>
+                <label>End date (return date): </label>
             </form>
-            <h1> Book a Car </h1>
         </div>
     );
 }
---
+++ src/pages/Location.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Location.css';
+
+/*
+Lisakhanya Zumana (230864821)
+Date: 06/06/2025
+ */
+
+function Location() {
+    return (
+        <div>
+            <h1>Register Car Rental Location (both pick up and Return)</h1>
+        </div>
+    );
+}
