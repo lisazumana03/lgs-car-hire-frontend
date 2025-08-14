767d537284b41ba0bc8849549f6c0704eb3fee37
lisazumana03 <zumanalisa@gmail.com>
2025-08-14 21:11:34 +0200

    file incoming

--- src/services/BookingService.js
+++ src/services/BookingService.js
@@ -1,3 +1,17 @@
+/*
+Lisakhanya Zumana (230864821)
+Date: 14/08/2025
+ */
+
 import axios from "axios";
 
-const API_URL = "http://localhost:2931/api/booking";
+const API_URL = "http://localhost:3045/booking";
+
+export const create = (booking) => {
+    return axios.post(API_URL, booking);
+}
+
+export const getAllBookings = () => {
+    return axios.get(API_URL);
+}
+
---
+++ src/services/locationService.js
@@ -0,0 +1,6 @@
+/*
+Lisakhanya Zumana (230864821)
+Date: 14/08/2025
+ */
+
+const API_URL = "http://localhost:3045/location";
