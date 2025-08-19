9736d7e71386d631d5e4999e7296e94c9777370e
lisazumana03 <zumanalisa@gmail.com>
2025-08-19 22:55:09 +0200

    file incoming

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import './App.css'
 import Header from "./pages/Common/Header.jsx";
 import Footer from "./pages/Common/Footer.jsx";
-import Navigation from "./pages/Common/Navigation.jsx";
 
 
 function App() {
   return (
       <>
       <Header/>
-          <Navigation/>
-          <main>
-                <div className="container">
-                    <h2>Welcome to LG's Car Hire</h2>
-                    <p>Your one-stop solution for all your car rental needs.</p>
-                </div>
-          </main>
       <Footer/>
       </>
   );
