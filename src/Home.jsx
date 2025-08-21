eef5f4cdff116e095f0e94a0d5ab0ed64cba0d1e
lisazumana03 <zumanalisa@gmail.com>
2025-08-21 13:34:56 +0200

    file incoming

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 function Home(){
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 w-full h-64 bg-[url('https://source.unsplash.com/random/800x600')] bg-cover bg-center">
-            <p> The best place to rent a car by far.</p>
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 w-full h-64 bg-[url('')] bg-cover bg-center">
+            <p> The number one spot where you can rent a car of your dreams. </p>
         </div>
     );
 }
