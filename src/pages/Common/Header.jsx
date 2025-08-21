457c1298bae4ce68b96a60c2d696a67410b8fc51
lisazumana03 <zumanalisa@gmail.com>
2025-08-21 14:04:05 +0200

    file incoming

--- src/pages/Common/Header.jsx
+++ src/pages/Common/Header.jsx
@@ -1,6 +1,10 @@
+import React from 'react';
+import Navigation from './Navigation';
+
 function Header(){
         return(
         <header className="bg-red-800 text-white bold font-bold p-6 bg-primary-dark flex justify-between items-center">
+            <Navigation/>
             <h1 className="text-3xl font-brand">Welcome to LG's Car Hire</h1>
             <hr></hr>
         </header>
--- src/pages/Common/Navigation.jsx
+++ src/pages/Common/Navigation.jsx
@@ -1,16 +1,11 @@
 import React from "react";
 
 function Navigation() {
-
   return (
-    <nav className="navigation">
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-        <li><a href="/book-a-car">Make Booking</a></li>
-      </ul>
-    </nav>
+      <div className="flex">
+        <nav className="text-white p-4">
+        </nav>
+      </div>
   );
 }
 
