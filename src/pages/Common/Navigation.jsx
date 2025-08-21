d440788e0933d3b94b20ebc0bd0f43945a161d58
lisazumana03 <zumanalisa@gmail.com>
2025-08-21 23:40:50 +0200

    file incoming

--- src/pages/Common/Navigation.jsx
+++ src/pages/Common/Navigation.jsx
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 function Navigation() {
   return (
-      <div className="flex">
-        <nav className="text-white p-4">
-            <Link to="/" className="text-white-700 hover:text-blue-600">
-                Home
-            </Link>
-            <Link to="/make-booking" className="text-white-700 hover:text-blue-600">
-                Make Booking
-            </Link>
-        </nav>
-      </div>
+    <div className="flex">
+      <nav className="flex flex-col text-white p-4 space-y-2">
+        <Link to="/" className="text-white-700 hover:text-blue-600">
+          Home
+        </Link>
+        <Link to="/make-booking" className="text-white-700 hover:text-blue-600">
+          Make Booking
+        </Link>
+      </nav>
+    </div>
   );
 }
 
