df3bcf756cd64b98ab4ba9db788dd5e192ede569
lisazumana03 <zumanalisa@gmail.com>
2025-08-21 11:08:45 +0200

    file incoming

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,24 @@ module.exports = {
         "./src/**/*.{js,jsx,ts,tsx}", // Make Tailwind scan all your files
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: '#1E40AF', // Blue
+                    light: '#3B82F6', // Light Blue
+                    dark: '#1E3A8A', // Dark Blue
+                },
+                secondary: {
+                    DEFAULT: '#F59E0B', // Yellow
+                    light: '#FCD34D', // Light Yellow
+                    dark: '#B45309', // Dark Yellow
+                }
+            },
+            fontFamily: {
+                sans: ['Arial', 'sans-serif'], // Use Inter font for sans-serif
+                serif: ['Merriweather', 'serif'], // Use Merriweather font for serif
+            }
+        },
     },
     plugins: [],
 }
