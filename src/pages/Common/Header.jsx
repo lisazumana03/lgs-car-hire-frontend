6a4cee1aed3f367d5609fa32cf0977f27fad5ff9
lisazumana03 <zumanalisa@gmail.com>
2025-08-21 09:07:25 +0200

    file incoming

--- src/pages/Common/Header.jsx
+++ src/pages/Common/Header.jsx
@@ -1,7 +1,7 @@
 function Header(){
         return(
-        <header className="text-4xl font-bold text-blue-600">
-            <h1>Welcome to LG's Car Hire</h1>
+        <header className="p-6 bg-primary-dark flex justify-between items-center">
+            <h1 className={"text-3xl font-brand"}>Welcome to LG's Car Hire</h1>
             <hr></hr>
         </header>
     );
