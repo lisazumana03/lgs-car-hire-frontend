9819130f48edb1627931c3ffcdae07dfe9e71167
lisazumana03 <zumanalisa@gmail.com>
2025-08-25 13:57:17 +0200

    file incoming

--- src/pages/Reservation/Location/LocationList.jsx
+++ src/pages/Reservation/Location/LocationList.jsx
@@ -2,9 +2,21 @@
 Lisakhanya Zumana (230864821)
 Date: 24/08/2025
  */
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { getAllLocations } from "../../../services/locationService";
 
 function LocationList() {
-  return (
+    const [locations, setLocations] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        getAllLocations()
+            .then(res => setLocations(res.data))
+            .catch(() => setLocations([]));
+    }, []);
+
+    return (
       <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50">
           <table>
                 <thead>
@@ -19,7 +31,20 @@ function LocationList() {
                     </tr>
                 </thead>
                 <tbody>
-                    {/* Map through locations and display them here */}
+                {locations.map(loc => (
+                    <tr key={loc.locationID}>
+                        <td className="px-4 py-2 border">{loc.locationName}</td>
+                        <td className="px-4 py-2 border">{loc.streetName}</td>
+                        <td className="px-4 py-2 border">{loc.cityOrTown}</td>
+                        <td className="px-4 py-2 border">{loc.provinceOrState}</td>
+                        <td className="px-4 py-2 border">{loc.country}</td>
+                        <td className="px-4 py-2 border">{loc.postalCode}</td>
+                        <td className="px-4 py-2 border">
+                            <button className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-700 mr-2">Edit</button>
+                            <button className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-700">Delete</button>
+                        </td>
+                    </tr>
+                ))}
                 </tbody>
           </table>
           <button type="button" className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-700" onClick={() => navigate("/")}>Back</button>
