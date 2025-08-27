2334da5ce573b56d5cafaf8f164a896324c9a614
lisazumana03 <zumanalisa@gmail.com>
2025-08-27 17:55:25 +0200

    file incoming

--- src/pages/Authentication/AdminDashboard.jsx
+++ src/pages/Authentication/AdminDashboard.jsx
@@ -1,7 +1,13 @@
+import {Link} from "react-router-dom";
+
 function AdminDashboard(){
     return(
         <div>
             <p> Welcome to the admin dashboard. </p>
+            <p> Here you can manage users, view reports, and configure system settings. </p>
+            <Link to="/register-location" className="mt-6 px-6 py-2 bg-red-600 text-white rounded shadow hover:bg-red-800 transition"> Register a Location </Link>
+            <Link to="/register-car" className="mt-6 px-6 py-2 bg-red-600 text-white rounded shadow hover:bg-red-800 transition"> Register a Car </Link>
+            <Link to="/queries" className="mt-6 px-6 py-2 bg-red-600 text-white rounded shadow hover:bg-red-800 transition"> Submit a Query </Link>
         </div>
     )
 }
