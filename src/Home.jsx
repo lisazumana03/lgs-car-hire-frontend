b5552f439f923f20c23b670eee892876977948f5
lisazumana03 <zumanalisa@gmail.com>
2025-08-27 13:21:13 +0200

    file incoming

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Home(){
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 w-full h-32 bg-[url('')] bg-cover bg-center">
+        <div className="">
             <p>The number one spot where you can rent a car of your dreams.</p>
             <Link to="/make-booking" className="mt-6 px-6 py-2 bg-red-600 text-white rounded shadow hover:bg-red-800 transition">Make Booking</Link>
             <Link to="/locations" className="mt-6 px-6 py-2 bg-red-600 text-white rounded shadow hover:bg-red-800 transition"> View Locations </Link>
