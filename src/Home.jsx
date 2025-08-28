d4e0d3fbbcec0fcd7be7936fb44de7eb7ab52966
lisazumana03 <zumanalisa@gmail.com>
2025-08-28 14:29:57 +0200

    file incoming

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ function Home(){
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-black-100 p-4">
             <p>The number one spot where you can rent a car of your dreams.</p>
-                <div className="flex flex-col space-y-4 w-full max-w-sm">
+                <div className="flex flex-wrap justify-center gap-4">
                     <Link to="/make-booking" className="mt-6 px-6 py-2 bg-red-600 text-white rounded shadow hover:bg-red-800 transition">Make Booking</Link>
                     <Link to="/locations" className="mt-6 px-6 py-2 bg-red-600 text-white rounded shadow hover:bg-red-800 transition"> View Locations </Link>
                     <Link to="/cars" className="mt-6 px-6 py-2 bg-red-600 text-white rounded shadow hover:bg-red-800 transition"> View Cars </Link>
