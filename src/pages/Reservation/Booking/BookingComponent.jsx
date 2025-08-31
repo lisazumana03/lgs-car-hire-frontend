5574027b53edf9e8e0518b7d5b112940a997717d
lisazumana03 <zumanalisa@gmail.com>
2025-08-31 20:12:24 +0200

    file incoming

--- src/pages/Reservation/Booking/BookingComponent.jsx
+++ src/pages/Reservation/Booking/BookingComponent.jsx
@@ -17,19 +17,19 @@ function bookingCreation(){
 export default function BookingComponent(){
     return (
         <div>
-            <h2>Bookings</h2>
+            <h2 className="text-center">Booking Options</h2>
             <div className="booking-links flex gap-4 mb-4">
                 <Link
-                    to="create"
-                    className="px-4 py-2 bg-blue-200 text-white rounded hover:bg-blue-800"
+                    to="/make-booking"
+                    className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-800"
                 >
-                    Create Booking
+                    Make Booking
                 </Link>
                 <Link
-                    to="history"
-                    className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-800"
+                    to="/booking-history"
+                    className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-800"
                 >
-                    Booking History
+                    View Booking History
                 </Link>
             </div>
 
